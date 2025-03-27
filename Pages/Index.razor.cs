6505805d3c7107430c93a98e556c3dbcd4003789
Ben Croughs <ben.croughs@telenet.be>
2025-03-27 09:04:32 +0100

    added radzendatepicker

---
+++ Pages/Index.razor.cs
@@ -0,0 +1,49 @@
+namespace RadzenDropdownSample.Pages;
+
+public partial class Index
+{
+    Dictionary<string, int ?> items = new Dictionary<string, int?>()
+    {
+        { "",null},
+        { "A", 1 },
+        { "B", 2 },
+        { "C", 3 },
+        { "D", 4 },
+        { "E", 5 },
+        { "F", 6 },
+        { "G", 7 },
+        { "H", 8 },
+        { "I", 9 },
+        { "J", 10 },
+        { "K", 11 },
+        { "L", 12 },
+        { "M", 13 },
+        { "N", 14 },
+        { "O", 15 },
+        { "P", 16 },
+        { "Q", 17 },
+        { "R", 18 },
+        { "S", 19 },
+        { "T", 20 },
+        { "U", 21 },
+        { "V", 22 },
+        { "W", 23 },
+        { "X", 24 },
+        { "Y", 25 },
+        { "Z", 26 }
+    };
+    
+    int? selectedItem;
+    public DateTime? Value { get; set; }
+
+    private void Callback(object obj)
+    {
+        Console.WriteLine(selectedItem);
+    }
+
+    private void OnChangeInner(DateTime? obj)
+    {
+        Value = obj;
+        Console.WriteLine(obj);
+    }
+}
